c1dabdfb5c3b18d4cb1079d1ddf44ff9e92c0d6e
Ross Wilkinson <ross.wilkinson@icloud.com>
2021-08-27 15:47:30 -0600

    update path to dataset

--- gripNoGrip/loadGngDatatset.py
+++ gripNoGrip/loadGngDatatset.py
@@ -10,7 +10,7 @@ Created on Fri Aug 27 14:30:42 2021
 import pandas as pd
 from pymer4.models import Lmer
 
-df = pd.read_csv('https://github.com/ross-wilkinson/datasets/blob/main/gripNoGrip.csv')
+df = pd.read_csv('https://github.com/ross-wilkinson/datasets/gripNoGrip/gripNoGrip.csv')
 df = df.rename(columns={0: "Subject", 1: "Condition", 2: "Trial", 3: "MaxPowerCycle"})
 
 model = Lmer('MaxPowerCycle ~ Condition + (1+Condition|Subject)', data=df)
