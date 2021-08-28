0497368c9faf6f861abf69e02dde4211f19679d1
ross-wilkinson <ross.wilkinson@icloud.com>
2021-08-28 17:38:18 -0600

    Update gripNoGrip.py

--- gripNoGrip/gripNoGrip.py
+++ gripNoGrip/gripNoGrip.py
@@ -11,7 +11,6 @@ import pandas as pd
 from pymer4.models import Lmer
 
 df = pd.read_csv('https://github.com/ross-wilkinson/datasets/gripNoGrip/gripNoGrip.csv')
-df = df.rename(columns={0: "Subject", 1: "Condition", 2: "Trial", 3: "MaxPowerCycle"})
 
 model = Lmer('MaxPowerCycle ~ Condition + (1+Condition|Subject)', data=df)
 mdf = model.fit()
